0f1361bd773c17aed04593c368e823c2d579ddf1
Brady Rudesill <bradyrudesill@gmail.com>
2015-10-30 00:26:19 -0700

    try to get response from venmo

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'coffee-rails', '~> 4.1.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
 gem 'aws-sdk', '< 2.0'
+gem "httparty"
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,19 @@
 class UsersController < ApplicationController
   def index
-    p "*" * 50
-    p request.original_url
-    p "*" * 50
+
+    # url = request.original_url
+    url = "https://dbcparlay.herokuapp.com/?code=785cf9fd022fe697215d59013323ac23"
+    authorization_code = url[38..-1]
+    p authorization_code
+    response = HTTParty.post("https://api.venmo.com/v1/oauth/access_token",{
+      "client_id": 3071,
+      "client_secret": aBDA9ba2eB4RZNcCJ8DRbbydhjsmXDBS,
+      "code": authorization_code
+      })
+
+    p "*" 50
+    p response
+    p "*" 50
   end
 
 end
