c3b2dd8912dd74a90f8f12ca8b7da6cf983fc08a
Brady Rudesill <bradyrudesill@gmail.com>
2015-10-30 00:53:31 -0700

    try to get response from venmo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,11 +7,12 @@ class UsersController < ApplicationController
     url = request.original_url
     authorization_code = url[38..-1]
     p authorization_code
-    response = HTTParty.post("https://api.venmo.com/v1/oauth/access_token",{
-      "client_id": "3071",
-      "client_secret": "aBDA9ba2eB4RZNcCJ8DRbbydhjsmXDBS",
-      "code": "authorization_code"
-      })
+    response = HTTParty.post("https://api.venmo.com/v1/oauth/access_token",
+      :body => { :client_id => '3071',
+                 :client_secret => 'aBDA9ba2eB4RZNcCJ8DRbbydhjsmXDBS',
+                 :code => "#{authorization_code}"
+      }.to_json,
+      :headers => { 'Content-Type' => 'application/json'} )
 
     p "*" * 50
     p response
