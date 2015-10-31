dfa9e46bff7ee4a1b20e0fb258378e7fbf4e78fb
Drew Peterson <drew.m.peterson@gmail.com>
2015-10-30 21:25:30 -0700

    add nested view

--- public/js/app.js
+++ public/js/app.js
@@ -5,18 +5,36 @@ app.config(function($stateProvider, $urlRouterProvider){
   $urlRouterProvider.otherwise('/')
 
   $stateProvider
-
     .state('new bet', {
       url: '/',
       controller: 'MainController',
       templateUrl: 'views/NewBet.html'
     })
 
-    .state('profile', {
-      url: '/profile',
+    // Nessted routes for the profile page...
+    .state('history', {
+      url: '/profile/history',
       controller: 'MainController',
-      templateUrl: 'views/profile.html'
+      views: {
+        '': { templateUrl: 'views/profile.html' },
+
+        'template@history': { templateUrl: 'views/profile-templates/_history.html'}
+      }
     })
+
+    .state('active', {
+      url: '/profile/active',
+      controller: 'MainController',
+      views: {
+        '': { templateUrl: 'views/profile.html' },
+
+        'template@active': { templateUrl: 'views/profile-templates/_active.html'}
+      }
+    })
+
+
+
+
 
 });
 
