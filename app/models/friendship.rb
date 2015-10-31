018f7690346bd63397bdf3568811e58daba01892
Joji Gorden <joji.gorden@gmail.com>
2015-10-31 09:09:51 -0700

    comment out some associations

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -4,9 +4,9 @@ class Friendship < ActiveRecord::Base
   belongs_to :user
   belongs_to :friend, :class_name => 'User'
   has_many :bets
-  def members
-    [self.friend, self.user]
-  end
+  # def members
+  #   [self.friend, self.user]
+  # end
 
   # def self.blah
   # end
