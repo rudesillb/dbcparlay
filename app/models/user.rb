760ce76f249284b6b67999aab6ad01ae36988f48
Joji Gorden <joji.gorden@gmail.com>
2015-10-31 10:11:33 -0700

    inverse associations set

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   has_many :bets, through: :friendships
   has_many :friendships, dependent: :destroy
   has_many :friends, through: :friendships
-  # has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
-  # has_many :inverse_friends, :through => :inverse_friendships, :source => :user
+  has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
+  has_many :inverse_bets, :through => :inverse_friendships, :source => :bets
 end
 # 47
--- public/js/app.js
+++ public/js/app.js
@@ -34,16 +34,16 @@ app.config(function($stateProvider, $urlRouterProvider){
 
 }); // END OF CONFIG
 
-$(function() {
-    // Slider on login page...
-    $( "#slider" ).slider({
-      range: "max",
-      min: 1,
-      max: 5,
-      value: 1,
-      slide: function( event, ui ) {
-        $( "#homeBetAmount" ).val( ui.value );
-      }
-    });
-});
+// $(function() {
+//     // Slider on login page...
+//     $( "#slider" ).slider({
+//       range: "max",
+//       min: 1,
+//       max: 5,
+//       value: 1,
+//       slide: function( event, ui ) {
+//         $( "#homeBetAmount" ).val( ui.value );
+//       }
+//     });
+// });
 
