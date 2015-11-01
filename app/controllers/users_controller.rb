856e6ca70c99c5b05a92fd01b5206fc0f0f1bcbf
Brady Rudesill <bradyrudesill@gmail.com>
2015-10-31 19:08:20 -0700

    Add pay_winner, cleaned up userscontroller and changed user to symbols

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
   def new
     url = request.original_url
     user = parse_user(send_response(url))
-    if User.find_by("venmo_id" => user["venmo_id"]) != nil
+    if User.find_by(:venmo_id => user[:venmo_id]) != nil
       login(user)
     else
       User.create(user)
@@ -14,28 +14,8 @@ class UsersController < ApplicationController
     user = {}
   end
 
-  private
-
-  def parse_user(response)
-    user = {}
-    user["access_token"] = response["access_token"]
-    user["email"] = response["user"]["email"]
-    user["first_name"] = response["user"]["first_name"]
-    user["last_name"] = response["user"]["last_name"]
-    user["username"] = response["user"]["username"]
-    user["venmo_id"] = response["user"]["id"]
-    return user
-  end
-
-  def send_response(url)
-    authorization_code = url[47..-1]
-    response = HTTParty.post("https://api.venmo.com/v1/oauth/access_token",
-      :body => { :client_id => ENV["VENMO_ID"],
-                 :client_secret => ENV["VENMO_SECRET"],
-                 :code => "#{authorization_code}"
-      }.to_json,
-      :headers => { 'Content-Type' => 'application/json'} )
-    return response
+  def destroy
+    logout
   end
 
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -12,4 +12,39 @@ module UsersHelper
     return nil unless session[:current_user_id]
     session[:current_user_id]
   end
+
+  def parse_user(response)
+    user = {}
+    user[:access_token] = response["access_token"]
+    user[:email] = response["user"]["email"]
+    user[:first_name] = response["user"]["first_name"]
+    user[:last_name] = response["user"]["last_name"]
+    user[:username] = response["user"]["username"]
+    user[:venmo_id] = response["user"]["id"]
+    return user
+  end
+
+  def send_response(url)
+    authorization_code = url[47..-1]
+    response = HTTParty.post("https://api.venmo.com/v1/oauth/access_token",
+      :body => { :client_id => ENV["VENMO_ID"],
+                 :client_secret => ENV["VENMO_SECRET"],
+                 :code => "#{authorization_code}"
+      }.to_json,
+      :headers => { 'Content-Type' => 'application/json'} )
+    return response
+  end
+
+  def pay_winner(bet)
+    winner = User.find_by(:venmo_id => bet[:winner])
+    loser = User.find_by(:venmo_id => current_user)
+
+    HTTParty.post("https://api.venmo.com/v1/payments",
+      :body => { :access_token => loser.access_token,
+                 :user_id      => loser[:venmo_id],
+                 :note         => bet.description,
+                 :amount       => bet.amount
+      }.to_json,
+      :headers => { 'Content-Type' => 'application/json'} )
+  end
 end
