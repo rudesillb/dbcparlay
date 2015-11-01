257f78ad37c265cb2b13e506538c00fb97487600
Brady Rudesill <bradyrudesill@gmail.com>
2015-10-31 17:58:29 -0700

    Change user helper

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,4 @@
+require 'active_support/all'
 class UsersController < ApplicationController
   skip_before_action :verify_authenticity_token
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,3 @@
 module ApplicationHelper
 
-  def login(user)
-    session[:current_user_id] = user["venmo_id"]
-  end
-
-  def logout
-    session.destroy
-  end
-
-  def current_user
-    return nil unless session[:current_user_id]
-    session[:current_user_id]
-  end
-
 end
