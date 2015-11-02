cbe89cd365f970ddbea6b59e913833d9cc23304b
Brady Rudesill <bradyrudesill@gmail.com>
2015-11-02 10:37:40 -0800

    Set session manually

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,19 +6,23 @@ class UsersController < ApplicationController
 
 
   def new
-    url = request.original_url
-    user = parse_user(send_response(url))
-    if User.find_by(:venmo_id => user[:venmo_id]) != nil
-      login(user)
-    else
-      User.create(user)
-      login(user)
-    end
-    user = {}
+    user = {id: 1, username: "Brady-Rudesill", first_name: "Brady", last_name: "Rudesill", email: "bradyrudesill@gmail.com", encrypted_access_token: "c6EklGYEtfrAwJaly1f1Ozg5+k+11DiJI86VlK5YwY2g26Pb+gBWpzGEEXx9\nVBG3p4dd0t/7ighIcL7PVLtO3aGZL5gsj+knItxZvfiyTl8=\n", encrypted_access_token_salt: "7f933fd724ca286c", encrypted_access_token_iv: "7U9Dl8x5ceM8ZYEGaO5XtA==\n", venmo_id: "1806884898078720454", large_image: "http://www.gravatar.com/avatar/6f1ee73002f145a28e6d559fc53347ad?d=mm&s=200", small_image: "http://www.gravatar.com/avatar/6f1ee73002f145a28e6d559fc53347ad?d=mm&s=80", created_at: "Mon, 02 Nov 2015 02:40:11 UTC +00:00", updated_at: "Mon, 02 Nov 2015 02:40:11 UTC +00:00"}
+    login(user)
+    print "Session:"
+    p session
+    # url = request.original_url
+    # user = parse_user(send_response(url))
+    # if User.find_by(:venmo_id => user[:venmo_id]) != nil
+    #   login(user)
+    # else
+    #   User.create(user)
+    #   login(user)
+    # end
+    # user = {}
   end
 
-  def destroy
-    logout
-  end
+  # def destroy
+  #   logout
+  # end
 
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -6,9 +6,9 @@ module UsersHelper
     session[:current_user_id] = user[:venmo_id]
   end
 
-  def logout
-    session.destroy
-  end
+  # def logout
+  #   session.destroy
+  # end
 
   def current_user
     return nil unless session[:current_user_id]
