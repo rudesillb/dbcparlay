e7e6d03358a8370964f7fa2cce2d6c1a0e1e0b47
Brady Rudesill <bradyrudesill@gmail.com>
2015-11-02 20:50:27 -0800

    print to console for heroku

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,12 +10,9 @@ class UsersController < ApplicationController
     # user = {id: 2, username: "grant-wong", first_name: "Grant", last_name: "Wong", email: "hoolaxin@gmail.com", encrypted_access_token: "yiLLQdhMyP70D0a82+E5AdsF3WRwP1FIZ9GVJ8jMk4E9sh4Qmm+IAiENSFZW\naU7tIh+qNBUutuHqcQPolV6jG2q4D1QbiglC4kFfOjKbWWY=\n", encrypted_access_token_salt: "d6a93f5a9b8a48e4", encrypted_access_token_iv: "sZs7NV1FZ1JwirP2VjPrmQ==\n", venmo_id: "1477224414838784567", large_image: "http://www.gravatar.com/avatar/497837f655bc04102bab81bb09b5efcb?d=mm&s=200", small_image: "http://www.gravatar.com/avatar/497837f655bc04102bab81bb09b5efcb?d=mm&s=80", created_at: "Mon, 02 Nov 2015 02:40:58 UTC +00:00", updated_at: "Mon, 02 Nov 2015 02:40:58 UTC +00:00"}
     # user = User.find_by(venmo_id: "1806884898078720454")
 
-    # login(user)
-    # get_friends(user)
     url = request.original_url
     user = parse_user(send_response(url))
     if User.find_by(:venmo_id => user[:venmo_id]) != nil
-      # user[:access_token] = response["access_token"]
       login(user)
       get_friends(user)
     else
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -17,7 +17,7 @@ module UsersHelper
 
   def authenticate
     if current_user == nil
-      redirect "https://api.venmo.com/v1/oauth/authorize?client_id=3073&scope=make_payments%20access_profile%20access_email%20access_phone%20access_balance&response_type=code"
+      redirect "https://api.venmo.com/v1/oauth/authorize?client_id=3073&scope=make_payments%20access_profile%20access_email%20access_phone%20access_friends%20access_balance&response_type=code"
     end
   end
 
